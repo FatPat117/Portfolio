a5834e8d7b025adbd011a61966d112e56756c8a7
Phat117 <ntphat110705@gmail.com>
2025-08-01 20:56:56 +0700

    Set up Theme color and Animation

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 export default function PageLayout() {
         return (
-                <div>
+                <div className="min-h-screen max-w-screen-xl mx-auto overflow-y-auto">
+                        {/* Theme Toggle */}
+
+                        {/* Background Effects */}
+
+                        {/* Navbar */}
+
+                        {/* Main content */}
                         <Outlet />
+
+                        {/* Footer */}
                 </div>
         );
 }
