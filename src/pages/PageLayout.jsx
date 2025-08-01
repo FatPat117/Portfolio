a2e3d3597f1cdc742cc5403b16e3e08c91130c8f
Phat117 <ntphat110705@gmail.com>
2025-08-01 21:43:28 +0700

    Setup Index.css

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 export default function PageLayout() {
         return (
-                <div className="min-h-screen max-w-screen-xl mx-auto overflow-y-auto">
+                <div className="min-h-screen max-w-screen-xl mx-auto overflow-y-auto bg-background text-color over-flow-x-hidden ">
                         {/* Theme Toggle */}
 
                         {/* Background Effects */}
