d68b83a9a85c77dac6000c7d59904d8e03c3a46e
Phat117 <ntphat110705@gmail.com>
2025-08-02 23:00:40 +0700

    About Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import AboutSection from "../components/AboutSection";
 import HeroSection from "../components/HeroSection";
-
 export default function Home() {
         return (
                 <>
                         <HeroSection />
+                        <AboutSection />
                 </>
         );
 }
