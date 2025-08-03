927a247b64c91d5c4b2ec20ccee2af636133dc87
Phat117 <ntphat110705@gmail.com>
2025-08-03 23:39:00 +0700

    Finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
+import { ArrowUpIcon } from "lucide-react";
 import React from "react";
 
 const Footer = () => {
         return (
-                <footer className="bg-background text-foreground py-8">
-                        <div className="container mx-auto">
-                                <p className="text-center">
-                                        &copy; {new Date().getFullYear()} Nguyen Tan Phat. All rights reserved.
-                                </p>
-                        </div>
+                <footer className="py-12 px-4  relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+                        <p className="text-center">
+                                &copy; {new Date().getFullYear()} Nguyen Tan Phat. All rights reserved.
+                        </p>
+
+                        <a href="#hero" className="p-2 rounded-full bg-primary hover:bg-primary/80 transition-colors">
+                                <ArrowUpIcon />
+                        </a>
                 </footer>
         );
 };
--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -6,20 +6,21 @@ import StarBackground from "../components/StarBackground";
 import ThemeToggle from "../components/ThemeToggle";
 export default function PageLayout() {
         return (
-                <div className="container min-h-screen  mx-auto overflow-y-auto bg-background text-color over-flow-x-hidden ">
-                        {/* Theme Toggle */}
-                        <ThemeToggle />
-                        {/* Background Effects */}
-                        <StarBackground />
-                        {/* Navbar */}
-                        <Navbar />
-                        {/* Main content */}
-                        <main>
-                                <Outlet />
-                        </main>
-
+                <>
+                        <div className="container min-h-screen  mx-auto overflow-y-auto bg-background text-color over-flow-x-hidden ">
+                                {/* Theme Toggle */}
+                                <ThemeToggle />
+                                {/* Background Effects */}
+                                <StarBackground />
+                                {/* Navbar */}
+                                <Navbar />
+                                {/* Main content */}
+                                <main>
+                                        <Outlet />
+                                </main>
+                        </div>
                         {/* Footer */}
                         <Footer />
-                </div>
+                </>
         );
 }
